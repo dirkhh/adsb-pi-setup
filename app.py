9b186a78b8cb83afc9492dd8fdc391df7d976dae
Dirk Hohndel <dirk@hohndel.org>
2023-04-19 20:40:38 +0100

    get timezone from browser
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+import subprocess
+from os import urandom
+
+
+def restart():
+    subprocess.call("/usr/bin/systemctl restart adsb-docker", shell=True)
+
+
+browser_timezone: str = 'Europe/Berlin'
 
 app = Flask(__name__)
+app.secret_key = urandom(16).hex()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/propagateTZ')
+def get_tz():
+    brower_timezone = request.args.get("tz")
+    print(f'get_tz called with {browser_timezone}')
+    return render_template('index.html', browser_timezone=browser_timezone)
 
-@app.route('/adsb-setup', methods=('GET', 'POST'))
+@app.route('/', methods=('GET', 'POST'))
 def setup():
     if request.method == 'POST':
         lat = request.form['lat']
         lng = request.form['lng']
         alt = request.form['alt']
-
-        if not lat:
-            flash('Latitude is required!')
-        if not lng:
-            flash('Longitude is required!')
-        if not alt:
-            flash('Altitude is required!')
+        form_timezone = request.form['form_timezone']
 
         if lat and lng and alt:
             # write local config file
-            with open('/opt/adsb/.env', 'w') as env_file:
-                env_file.write('# adsb-pi feeder environment, written by the web config')
-                env_file.write(f"FEEDER_LAT={lat}")
-                env_file.write(f"FEEDER_LONG={lng}")
-                env_file.write(f"FEEDER_ALT_M={alt}")
-            return render_template('alldone.html')
+            with open('/opt/adsb/.web-setup.env', 'w') as env_file:
+                env_file.write('# adsb-pi feeder environment, written by the web config\n')
+                env_file.write(f"FEEDER_LAT={lat}\n")
+                env_file.write(f"FEEDER_LONG={lng}\n")
+                env_file.write(f"FEEDER_ALT_M={alt}\n")
+                env_file.write(f"FEEDER_TZ={form_timezone}\n")
+            restart()
 
-    return render_template('setup.html')
+    return render_template('index.html', browser_timezone=browser_timezone)
