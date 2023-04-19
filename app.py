36555dc143f85d4f613a25ff5b05bcf29c9d682f
Dirk Hohndel <dirk@hohndel.org>
2023-04-19 21:14:30 +0100

    Work with the env files to fill the form
    Make sure all data is provided before the user submits.
    
    Also implement a little waiting screen.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import subprocess
-from os import urandom
+from os import urandom,path
 
+
+system_file: str = "/opt/adsb/.env"
+web_file: str = "/opt/adsb/.web-setup.env"
+
+def parse_env_files():
+    _env_values = {}
+    for env_file in [ system_file, web_file ]:
+        if not path.isfile(env_file):
+            continue
+        with open(env_file) as f:
+            for line in f:
+                if line.strip().startswith('#'):
+                    continue
+                key, var = line.partition("=")[::2]
+                _env_values[key.strip()] = var.strip()
+    return _env_values
 
 def restart():
     subprocess.call("/usr/bin/systemctl restart adsb-docker", shell=True)
@@ -16,24 +32,39 @@ app.secret_key = urandom(16).hex()
 def get_tz():
     brower_timezone = request.args.get("tz")
     print(f'get_tz called with {browser_timezone}')
-    return render_template('index.html', browser_timezone=browser_timezone)
+    env_values = parse_env_files()
+    env_values['FEEDER_TZ'] = browser_timezone
+    return render_template('index.html', env_values=env_values)
+
+@app.route('/restarting', methods=('GET', 'POST'))
+def restarting():
+    if request.method == 'POST':
+        restart()
+        return "done"
+    if request.method == 'GET':
+        return render_template('restarting.html')
 
 @app.route('/', methods=('GET', 'POST'))
 def setup():
+    message = ""
+    if request.args.get("success"):
+        message = "Restarting the ADSB app completed"
+    env_values = parse_env_files()
+
     if request.method == 'POST':
         lat = request.form['lat']
         lng = request.form['lng']
         alt = request.form['alt']
         form_timezone = request.form['form_timezone']
 
-        if lat and lng and alt:
+        if lat and lng and alt and form_timezone:
             # write local config file
-            with open('/opt/adsb/.web-setup.env', 'w') as env_file:
+            with open(web_file, 'w') as env_file:
                 env_file.write('# adsb-pi feeder environment, written by the web config\n')
                 env_file.write(f"FEEDER_LAT={lat}\n")
                 env_file.write(f"FEEDER_LONG={lng}\n")
                 env_file.write(f"FEEDER_ALT_M={alt}\n")
                 env_file.write(f"FEEDER_TZ={form_timezone}\n")
-            restart()
+            return redirect('/restarting')
 
-    return render_template('index.html', browser_timezone=browser_timezone)
+    return render_template('index.html', env_values=env_values, message=message)
