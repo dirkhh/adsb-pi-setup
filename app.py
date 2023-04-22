7fb9d40e141ddf66e93869a392b1ab704d1a97ca
Dirk Hohndel <dirk@hohndel.org>
2023-04-22 03:35:29 +0000

    add optional setup for feeder
    Select whether to show routes and decide which aggregators to feed.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import subprocess
 from os import urandom,path
+import re
 
 
 system_file: str = "/opt/adsb/.env"
 web_file: str = "/opt/adsb/.web-setup.env"
+adv_file: str = "/opt/adsb/.adv-setup.env"
 
 def parse_env_files():
     _env_values = {}
-    for env_file in [ system_file, web_file ]:
+    for env_file in [ system_file, web_file, adv_file ]:
         if not path.isfile(env_file):
             continue
         with open(env_file) as f:
@@ -17,6 +19,9 @@ def parse_env_files():
                     continue
                 key, var = line.partition("=")[::2]
                 _env_values[key.strip()] = var.strip()
+    if 'READSB_NET_CONNECTOR' not in _env_values: _env_values['READSB_NET_CONNECTOR'] = ''
+    if 'MLAT_CONFIG' not in _env_values: _env_values['MLAT_CONFIG'] = ''
+    if 'route' not in _env_values: _env_values['route'] = ''
     return _env_values
 
 def restart():
@@ -44,9 +49,81 @@ def restarting():
     if request.method == 'GET':
         return render_template('restarting.html')
 
+@app.route('/advanced', methods=('GET', 'POST'))
+def advanced():
+    if request.method == 'POST':
+        # explicitly make these empty
+        route = mlat = net = ''
+        if request.form.get('route', True):
+            route = '1'
+        if request.form.get('adsblol', True):
+            if net: net += ';'
+            if mlat: mlat += ';'
+            net += 'in.adsb.lol,30004,beast_reduce_plus_out'
+            mlat += 'in.adsb.lol,31090,39001'
+        if request.form.get('adsbone', False):
+            if net: net += ';'
+            if mlat: mlat += ';'
+            net += 'feed.adsb.one,64004,beast_reduce_plus_out'
+            mlat += 'feed.adsb.one,64006,39002'
+        if request.form.get('adsbfi', False):
+            if net: net += ';'
+            if mlat: mlat += ';'
+            net += 'feed.adsb.fi,30004,beast_reduce_plus_out'
+            mlat += 'feed.adsb.fi,31090,39000'
+        if request.form.get('adsbx', False):
+            if net: net += ';'
+            if mlat: mlat += ';'
+            net += 'feed1.adsbexchange.com,30004,beast_reduce_plus_out'
+            mlat += 'feed.adsbexchange.com,31090,39005'
+        if request.form.get('tat', True):
+            if net: net += ';'
+            if mlat: mlat += ';'
+            net += 'feed.theairtraffic.com,30004,beast_reduce_plus_out'
+            mlat += 'feed.theairtraffic.com,31090,39004'
+        if request.form.get('ps', True):
+            if net: net += ';'
+            if mlat: mlat += ';'
+            net += 'feed.planespotters.net,30004,beast_reduce_plus_out'
+            mlat += 'mlat.planespotters.net,31090,39003'
+        if mlat == '':   # things fail if those variables are empty
+            net = 'in.adsb.lol,30004,beast_reduce_plus_out'
+            mlat = 'in.adsb.lol,31090,39001'
+
+        updated = []
+        with open(adv_file, 'r') as afile:
+            for line in afile:
+                match = re.search('^[^#]*TAR1090_USEROUTEAPI[ \t]*=', line)
+                if match:
+                    continue
+                match = re.search('^[^#]*READSB_NET_CONNECTOR[ \t]*=', line)
+                if match:
+                    continue
+                match = re.search('^[^#]*MLAT_CONFIG[ \t]*=', line)
+                if match:
+                    continue
+                updated.append(line)
+        updated.append(f"TAR1090_USEROUTEAPI = {route}")
+        updated.append(f"READSB_NET_CONNECTOR = {net}")
+        updated.append(f"MLAT_CONFIG = {mlat}")
+        with open(adv_file, 'w') as env_file:
+            for line in updated:
+                env_file.write(f"{line}\n")
+        return redirect('/restarting')
+    env_values = parse_env_files()
+    env_values['route'] = 'checked' if env_values['TAR1090_USEROUTEAPI'] != '' else ''
+    env_values['adsblol'] = 'checked' if 'adsb.lol' in env_values['READSB_NET_CONNECTOR'] else ''
+    env_values['adsbone'] = 'checked' if 'adsb.one' in env_values['READSB_NET_CONNECTOR'] else ''
+    env_values['adsbfi'] = 'checked' if 'adsb.fi' in env_values['READSB_NET_CONNECTOR'] else ''
+    env_values['adsbx'] = 'checked' if 'adsbexchange' in env_values['READSB_NET_CONNECTOR'] else ''
+    env_values['tat'] = 'checked' if 'theairtraffic' in env_values['READSB_NET_CONNECTOR'] else ''
+    env_values['ps'] = 'checked' if 'planespotters' in env_values['READSB_NET_CONNECTOR'] else ''
+    return render_template('advanced.html', env_values=env_values)
+            
+
 @app.route('/', methods=('GET', 'POST'))
 def setup():
-    message = ""
+    message = ''
     if request.args.get("success"):
         # the message most likely will never be seen, because we immediately
         # redirect the user to the tar1090 website
@@ -55,7 +132,6 @@ def setup():
         tar1090 = request.url_root.replace(str(port), "8080")
         return redirect(tar1090)
     env_values = parse_env_files()
-
     if request.method == 'POST':
         lat = request.form['lat']
         lng = request.form['lng']
