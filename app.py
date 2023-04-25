17b8a7710213c8380d549b0e3100ec7bf729af9c
Katia <katia@linux.com>
2023-04-25 18:35:00 +0200

    class

--- app.py
+++ app.py
@@ -1,59 +1,67 @@
 import re
 import subprocess
 import threading
-from os import path, urandom
+from os import path, urandom, getenv
 from typing import Dict
 
 from flask import Flask, g, redirect, render_template, request
 
-env_file = ".env"
 
+class EnvFile:
+    def __init__(self, env_file_path: str):
+        self.env_file_path = env_file_path
 
-def parse_env_file():
-    env_values = {}
-    with open(env_file) as f:
-        for line in f:
-            if line.strip().startswith("#"):
-                continue
-            key, var = line.partition("=")[::2]
-            env_values[key.strip()] = var.strip()
+    def _setup(self):
+        # if file does not exist, create it
+        if not path.isfile(self.env_file_path):
+            open(self.env_file_path, "w").close()
 
-    env_values.setdefault("FEEDER_TAR1090_USEROUTEAPI", "")
-    env_values.setdefault("FEEDER_ULTRAFEEDER_CONFIG", "")
-    env_values.setdefault("route", "")
+    @property
+    def envs(self):
+        env_values = {}
 
-    adv_visibility = (
-        "visible"
-        if all(
-            key in env_values and float(env_values[key]) != 0
-            for key in ["FEEDER_LAT", "FEEDER_LONG", "FEEDER_ALT"]
+        with open(self.env_file_path) as f:
+            for line in f:
+                if line.strip().startswith("#"):
+                    continue
+                key, var = line.partition("=")[::2]
+                env_values[key.strip()] = var.strip()
+
+        env_values.setdefault("FEEDER_TAR1090_USEROUTEAPI", "")
+        env_values.setdefault("FEEDER_ULTRAFEEDER_CONFIG", "")
+        env_values.setdefault("route", "")
+
+        adv_visibility = (
+            "visible"
+            if all(
+                key in env_values and float(env_values[key]) != 0
+                for key in ["FEEDER_LAT", "FEEDER_LONG", "FEEDER_ALT"]
+            )
+            else "invisible"
         )
-        else "invisible"
-    )
-    env_values["adv_visible"] = adv_visibility
+        env_values["adv_visible"] = adv_visibility
 
-    return env_values
+        return env_values
 
+    def modify_env(self, values: Dict[str, str]):
+        if not path.isfile(self.env_file_path):
+            open(self.env_file_path, "w").close()
 
-def modify_env(values: Dict[str, str]):
-    if not path.isfile(env_file):
-        open(env_file, "w").close()
+        with open(self.env_file_path, "r") as ef:
+            lines = ef.readlines()
 
-    with open(env_file, "r") as ef:
-        lines = ef.readlines()
+        updated_values = values.copy()
+        for idx, line in enumerate(lines):
+            for key in values.keys():
+                match = re.search(f"(^[^#]*{key}[^#]*=)[^#]*", line)
+                if match:
+                    lines[idx] = f"{match.group(1)} {values[key]}\n"
+                    updated_values.pop(key, None)
 
-    updated_values = values.copy()
-    for idx, line in enumerate(lines):
-        for key in values.keys():
-            match = re.search(f"(^[^#]*{key}[^#]*=)[^#]*", line)
-            if match:
-                lines[idx] = f"{match.group(1)} {values[key]}\n"
-                updated_values.pop(key, None)
+        lines.extend(f"{key} = {value}" for key, value in updated_values.items())
 
-    lines.extend(f"{key} = {value}" for key, value in updated_values.items())
-
-    with open(env_file, "w") as ef:
-        ef.writelines(lines)
+        with open(self.env_file_path, "w") as ef:
+            ef.writelines(lines)
 
 
 class Restart:
@@ -68,20 +76,17 @@ class Restart:
             subprocess.call("/usr/bin/systemctl restart adsb-docker", shell=True)
 
 
-def get_restart():
-    if "restart" not in g:
-        g.restart = Restart()
-    return g.restart
-
-
 app = Flask(__name__)
 app.secret_key = urandom(16).hex()
+g.env_file = EnvFile(getenv("ADSB_PI_SETUP_ENVFILE", ".env"))
+g.env_file._setup()
+g.restart = Restart()
 
 
 @app.route("/propagateTZ")
 def get_tz():
     browser_timezone = request.args.get("tz")
-    env_values = parse_env_file()
+    env_values = g.env_file.envs
     env_values["FEEDER_TZ"] = browser_timezone
     return render_template("index.html", env_values=env_values)
 
@@ -93,14 +98,14 @@ def restarting():
         if not restart:
             return "restarting"
         return "done"
-    return render_template("restarting.html", env_values=parse_env_file())
+    return render_template("restarting.html", env_values=g.env_file.envs)
 
 
 @app.route("/advanced", methods=("GET", "POST"))
 def advanced():
     if request.method == "POST":
         return handle_advanced_post_request()
-    env_values = parse_env_file()
+    env_values = g.env_file.envs
     set_checked_flags(env_values)
     return render_template("advanced.html", env_values=env_values)
 
@@ -117,7 +122,7 @@ def handle_advanced_post_request():
             net_configs.append(net_config)
     net = ";".join(net_configs) or DEFAULT_NET_CONFIG
 
-    modify_env(
+    g.env_file.modify_env(
         {
             "FEEDER_TAR1090_USEROUTEAPI": "1" if request.form.get("route") else "0",
             "FEEDER_ULTRAFEEDER_CONFIG": net,
@@ -152,15 +157,13 @@ def setup():
     if request.args.get("success"):
         return redirect("/advanced")
 
-    env_values = parse_env_file()
-
     if request.method == "POST":
         lat, lng, alt, form_timezone = (
             request.form[key] for key in ["lat", "lng", "alt", "form_timezone"]
         )
 
         if all([lat, lng, alt, form_timezone]):
-            modify_env(
+            g.env_file.modify_env(
                 {
                     "FEEDER_LAT": lat,
                     "FEEDER_LONG": lng,
@@ -170,4 +173,4 @@ def setup():
             )
             return redirect("/restarting")
 
-    return render_template("index.html", env_values=env_values)
+    return render_template("index.html", env_values=g.env_file.envs)
