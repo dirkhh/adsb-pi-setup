293b876bd4f7bc5ebccb9def3c0884119efa2bfa
Katia <katia@linux.com>
2023-04-25 17:13:51 +0200

    refactoring

--- app.py
+++ app.py
@@ -1,150 +1,173 @@
+import re
+import subprocess
+import threading
+from os import path, urandom
 from typing import Dict
 
-from flask import Flask, render_template, request, redirect
-import subprocess
-from os import urandom, path
-import re
+from flask import Flask, g, redirect, render_template, request
 
-
-env_file: str = ".env"
+env_file = ".env"
 
 
 def parse_env_file():
-    _env_values = {}
-    if path.isfile(env_file):
-        with open(env_file) as f:
-            for line in f:
-                if line.strip().startswith('#'):
-                    continue
-                key, var = line.partition("=")[::2]
-                _env_values[key.strip()] = var.strip()
-    if 'FEEDER_TAR1090_USEROUTEAPI' not in _env_values: _env_values['FEEDER_TAR1090_USEROUTEAPI'] = ''
-    if 'FEEDER_ULTRAFEEDER_CONFIG' not in _env_values: _env_values['FEEDER_ULTRAFEEDER_CONFIG'] = ''
-    if 'route' not in _env_values: _env_values['route'] = ''
-    if 'FEEDER_LAT' in _env_values and float(_env_values['FEEDER_LAT']) != 0.0 and \
-            'FEEDER_LONG' in _env_values and float(_env_values['FEEDER_LONG']) != 0.0 and \
-            'FEEDER_ALT' in _env_values and int(_env_values['FEEDER_ALT']) != 0:
-        _env_values['adv_visible'] = 'visible'
-    else:
-        _env_values['adv_visible'] = 'invisible'
+    env_values = {}
+    with open(env_file) as f:
+        for line in f:
+            if line.strip().startswith("#"):
+                continue
+            key, var = line.partition("=")[::2]
+            env_values[key.strip()] = var.strip()
 
-    return _env_values
+    env_values.setdefault("FEEDER_TAR1090_USEROUTEAPI", "")
+    env_values.setdefault("FEEDER_ULTRAFEEDER_CONFIG", "")
+    env_values.setdefault("route", "")
+
+    adv_visibility = (
+        "visible"
+        if all(
+            key in env_values and float(env_values[key]) != 0
+            for key in ["FEEDER_LAT", "FEEDER_LONG", "FEEDER_ALT"]
+        )
+        else "invisible"
+    )
+    env_values["adv_visible"] = adv_visibility
+
+    return env_values
 
 
-# read the .env file and update those mentioned in the Dict passed in (and add those entries that are new)
 def modify_env(values: Dict[str, str]):
     if not path.isfile(env_file):
-        # that's not a good sign, but at least let's not throw an error
-        ef = open(env_file, 'w')
-        ef.close()
-    with open(env_file, 'r') as ef:
+        open(env_file, "w").close()
+
+    with open(env_file, "r") as ef:
         lines = ef.readlines()
-        for idx in range(len(lines)):
-            line = lines[idx]
-            for key in values.keys():
-                match = re.search(f"(^[^#]*{key}[^#]*=)[^#]*", line)
-                if match:
-                    lines[idx] = f"{match.group(1)} {values[key]}\n"
-                    values[key] = ''  # so we don't write it a second time at the end
-    for key in values:
-        if values[key]:
-            lines.append(f"{key} = {values[key]}")
+
+    updated_values = values.copy()
+    for idx, line in enumerate(lines):
+        for key in values.keys():
+            match = re.search(f"(^[^#]*{key}[^#]*=)[^#]*", line)
+            if match:
+                lines[idx] = f"{match.group(1)} {values[key]}\n"
+                updated_values.pop(key, None)
+
+    lines.extend(f"{key} = {value}" for key, value in updated_values.items())
+
     with open(env_file, "w") as ef:
         ef.writelines(lines)
 
 
-def restart():
-    # this really needs to check if we are still waiting for the previous restart
-    subprocess.call("/usr/bin/systemctl restart adsb-docker", shell=True)
+class Restart:
+    def __init__(self):
+        self.lock = threading.Lock()
+
+    def restart_systemd(self):
+        # if locked, return immediately
+        if not self.lock.acquire(False):
+            return False
+        with self.lock:
+            subprocess.call("/usr/bin/systemctl restart adsb-docker", shell=True)
+
+
+def get_restart():
+    if "restart" not in g:
+        g.restart = Restart()
+    return g.restart
 
 
 app = Flask(__name__)
 app.secret_key = urandom(16).hex()
 
 
-@app.route('/propagateTZ')
+@app.route("/propagateTZ")
 def get_tz():
     browser_timezone = request.args.get("tz")
     env_values = parse_env_file()
-    env_values['FEEDER_TZ'] = browser_timezone
-    return render_template('index.html', env_values=env_values)
+    env_values["FEEDER_TZ"] = browser_timezone
+    return render_template("index.html", env_values=env_values)
 
 
-@app.route('/restarting', methods=('GET', 'POST'))
+@app.route("/restarting", methods=("GET", "POST"))
 def restarting():
-    if request.method == 'POST':
-        restart()
+    if request.method == "POST":
+        restart = get_restart().restart_systemd()
+        if not restart:
+            return "restarting"
         return "done"
-    if request.method == 'GET':
-        return render_template('restarting.html', env_values=parse_env_file())
+    return render_template("restarting.html", env_values=parse_env_file())
 
 
-@app.route('/advanced', methods=('GET', 'POST'))
+@app.route("/advanced", methods=("GET", "POST"))
 def advanced():
-    if request.method == 'POST':
-        if request.form.get('tar1090') == "go":
-            host, port = request.server
-            tar1090 = request.url_root.replace(str(port), "8080")
-            return redirect(tar1090)
-
-        # explicitly make these empty
-        route = 0
-        net = ''
-        if request.form.get('route'):
-            route = '1'
-        if request.form.get('adsblol'):
-            if net: net += ';'
-            net += 'adsb,in.adsb.lol,30004,beast_reduce_plus_out;mlat,in.adsb.lol,31090,39001'
-        if request.form.get('adsbone'):
-            if net: net += ';'
-            net += 'adsb,feed.adsb.one,64004,beast_reduce_plus_out;mlat,feed.adsb.one,64006,39002'
-        if request.form.get('adsbfi'):
-            if net: net += ';'
-            net += 'adsb,feed.adsb.fi,30004,beast_reduce_plus_out;mlat,feed.adsb.fi,31090,39000'
-        if request.form.get('adsbx'):
-            if net: net += ';'
-            net += 'adsb,feed1.adsbexchange.com,30004,beast_reduce_plus_out;mlat,feed.adsbexchange.com,31090,39005'
-        if request.form.get('tat'):
-            if net: net += ';'
-            net += 'adsb,feed.theairtraffic.com,30004,beast_reduce_plus_out;mlat,feed.theairtraffic.com,31090,39004'
-        if request.form.get('ps'):
-            if net: net += ';'
-            net += 'adsb,feed.planespotters.net,30004,beast_reduce_plus_out;mlat,mlat.planespotters.net,31090,39003'
-        if net == '':   # things fail if this variable is empty
-            net = 'adsb,in.adsb.lol,30004,beast_reduce_plus_out;mlat,in.adsb.lol,31090,39001'
-
-        modify_env({'FEEDER_TAR1090_USEROUTEAPI': route,
-                    'FEEDER_ULTRAFEEDER_CONFIG': net})
-        return redirect('/restarting')
+    if request.method == "POST":
+        return handle_advanced_post_request()
     env_values = parse_env_file()
-    env_values['route'] = 'checked' if env_values['FEEDER_TAR1090_USEROUTEAPI'] == '1' else ''
-    env_values['adsblol'] = 'checked' if 'adsb.lol' in env_values['FEEDER_ULTRAFEEDER_CONFIG'] else ''
-    env_values['adsbone'] = 'checked' if 'adsb.one' in env_values['FEEDER_ULTRAFEEDER_CONFIG'] else ''
-    env_values['adsbfi'] = 'checked' if 'adsb.fi' in env_values['FEEDER_ULTRAFEEDER_CONFIG'] else ''
-    env_values['adsbx'] = 'checked' if 'adsbexchange' in env_values['FEEDER_ULTRAFEEDER_CONFIG'] else ''
-    env_values['tat'] = 'checked' if 'theairtraffic' in env_values['FEEDER_ULTRAFEEDER_CONFIG'] else ''
-    env_values['ps'] = 'checked' if 'planespotters' in env_values['FEEDER_ULTRAFEEDER_CONFIG'] else ''
-    return render_template('advanced.html', env_values=env_values)
-            
+    set_checked_flags(env_values)
+    return render_template("advanced.html", env_values=env_values)
 
-@app.route('/', methods=('GET', 'POST'))
+
+def handle_advanced_post_request():
+    if request.form.get("tar1090") == "go":
+        host, port = request.server
+        tar1090 = request.url_root.replace(str(port), "8080")
+        return redirect(tar1090)
+
+    net_configs = []
+    for key, net_config in NET_CONFIGS.items():
+        if request.form.get(key):
+            net_configs.append(net_config)
+    net = ";".join(net_configs) or DEFAULT_NET_CONFIG
+
+    modify_env(
+        {
+            "FEEDER_TAR1090_USEROUTEAPI": "1" if request.form.get("route") else "0",
+            "FEEDER_ULTRAFEEDER_CONFIG": net,
+        }
+    )
+    return redirect("/restarting")
+
+
+NET_CONFIGS = {
+    "adsblol": "adsb,in.adsb.lol,30004,beast_reduce_plus_out;mlat,in.adsb.lol,31090,39001",
+    "adsbx": "adsb,feed1.adsbexchange.com,30004,beast_reduce_plus_out;mlat,feed.adsbexchange.com,31090,39005",
+    "tat": "adsb,feed.theairtraffic.com,30004,beast_reduce_plus_out;mlat,feed.theairtraffic.com,31090,39004",
+    "ps": "adsb,feed.planespotters.net,30004,beast_reduce_plus_out;mlat,mlat.planespotters.net,31090,39003",
+    "adsbone": "adsb,feed.adsb.one,64004,beast_reduce_plus_out;mlat,feed.adsb.one,64006,39002",
+    "adsbfi": "adsb,feed.adsb.fi,30004,beast_reduce_plus_out;mlat,feed.adsb.fi,31090,39000",
+}
+
+DEFAULT_NET_CONFIG = (
+    "adsb,in.adsb.lol,30004,beast_reduce_plus_out;mlat,in.adsb.lol,31090,39001"
+)
+
+
+def set_checked_flags(env_values):
+    for key in NET_CONFIGS.keys():
+        env_values[key] = (
+            "checked" if key in env_values["FEEDER_ULTRAFEEDER_CONFIG"] else ""
+        )
+
+
+@app.route("/", methods=("GET", "POST"))
 def setup():
-    message = ''
     if request.args.get("success"):
         return redirect("/advanced")
+
     env_values = parse_env_file()
-    if request.method == 'POST':
-        lat = request.form['lat']
-        lng = request.form['lng']
-        alt = request.form['alt']
-        form_timezone = request.form['form_timezone']
 
-        if lat and lng and alt and form_timezone:
-            # write local config file
-            modify_env({'FEEDER_LAT': lat,
-                        'FEEDER_LONG': lng,
-                        'FEEDER_ALT': alt,
-                        'FEEDER_TZ': form_timezone})
-            return redirect('/restarting')
+    if request.method == "POST":
+        lat, lng, alt, form_timezone = (
+            request.form[key] for key in ["lat", "lng", "alt", "form_timezone"]
+        )
 
-    return render_template('index.html', env_values=env_values, message=message)
+        if all([lat, lng, alt, form_timezone]):
+            modify_env(
+                {
+                    "FEEDER_LAT": lat,
+                    "FEEDER_LONG": lng,
+                    "FEEDER_ALT": alt,
+                    "FEEDER_TZ": form_timezone,
+                }
+            )
+            return redirect("/restarting")
+
+    return render_template("index.html", env_values=env_values)
